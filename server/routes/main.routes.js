d295da38429d1e474b726b4e68f0e7fbd0c65f74
Thecots <thecots22@gmail.com>
2022-01-25 11:47:05 +0100

    ejs v1

--- server/routes/main.routes.js
+++ server/routes/main.routes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const path = require('path');
 
 
-
+router.get('/', (req, res) => {
+  res.render("index", { title: "hello world" })
+})
 
 /* 404 */
-router.get('*', function (req, res) {
+router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/html/404/404.html'));
 });
 
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
 app.use(cors());
@@ -13,6 +14,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+
+/* ejs */
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname + '/views'))
 
 /* routes */
 app.use(require('./routes/main.routes'));
